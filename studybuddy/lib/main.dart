277c2f7469e872e4e45ba8d678979bb1147b7fe1
Cynthia Akanaga <cynthiaakanaga@gmail.com>
2023-10-28 00:57:40 -0400

    updated main.dart

--- studybuddy/lib/main.dart
+++ studybuddy/lib/main.dart
@@ -8,15 +8,16 @@ import 'package:studybuddy/src/features/authentication/screens/splash_screens/sp
 import 'package:studybuddy/src/features/authentication/screens/welcome_screens/welcome_screen.dart';
 import 'package:studybuddy/src/repository/authentication_repository/authentication_repository.dart';
 import 'package:studybuddy/src/utils/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'firebase_options.dart';
 
-void main() {
+void main() async {
   //ensures that init of all widgets has been before loading of app
   WidgetsFlutterBinding.ensureInitialized();
   //initializes firebase for the specific platform that the app is being run on
   //aka entry point for accessing firebase
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform)
   //then we initialize the authentication repository
       .then((value) => Get.put(AuthenticationRepository()));
   runApp(const MyApp());
