9efb37583fd5e22fe298a2298dba714740d6ecfa
Cynthia Akanaga <cynthiaakanaga@gmail.com>
2023-10-31 13:58:33 -0400

    changing errors

--- studybuddy/lib/src/features/authentication/screens/onboarding_screens/sign_up_screen.dart
+++ studybuddy/lib/src/features/authentication/screens/onboarding_screens/sign_up_screen.dart
@@ -1,20 +1,16 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
 
 import 'package:studybuddy/src/constants/sizes.dart';
-import 'package:studybuddy/src/features/authentication/screens/onboarding_screens/onboarding_screen.dart';
 import 'package:studybuddy/src/features/authentication/screens/widgets/form_header_widget.dart';
-import '../../../../constants/image_strings.dart';
+
 import '../../../../constants/text_strings.dart';
 import '../widgets/sign_up_form_two_widget.dart';
 import 'login_screen.dart';
 
 class SignUpScreen extends StatelessWidget {
   const SignUpScreen({Key? key}) : super(key: key);
-
 
   @override
   Widget build(BuildContext context) {
@@ -31,8 +27,7 @@ class SignUpScreen extends StatelessWidget {
               children: [
                 FormHeaderWidget(
                     sWelcomeTitle: sWelcomeTitle,
-                    sWelcomeSubtitle: sWelcomeSubtitle
-                ),
+                    sWelcomeSubtitle: sWelcomeSubtitle),
                 SignUpFormWidget(),
                 SizedBox(height: sFormHeight - 10),
                 Column(
@@ -43,44 +38,38 @@ class SignUpScreen extends StatelessWidget {
                     ),
                     SizedBox(
                       width: double.infinity,
-                      child: OutlinedButton.icon(
-                          onPressed: () async {
-                          User? user = await signInWithGoogle();
-                          if (user != null) {
-    // Successfully signed in with Google, do something with the user.
-                            print("Signed in with Google: ${user.displayName}");
-                          } else {
-    // Handle sign-in failure
-                          print("Google Sign-In failed.");
-                          }
-                          },
-                          icon: Image(image: AssetImage(
-                            sGoogleLogo),
-                            width: 20,
-                          ),
-                          label: Text(
-                              sLoginWithGoogle,
-
-                          )
-                      ),
+                      // child: OutlinedButton.icon(
+                      //     onPressed: () async {
+                      //       User? user = await signInWithGoogle();
+                      //       if (user != null) {
+                      //         // Successfully signed in with Google, do something with the user.
+                      //         print(
+                      //             "Signed in with Google: ${user.displayName}");
+                      //       } else {
+                      //         // Handle sign-in failure
+                      //         print("Google Sign-In failed.");
+                      //       }
+                      //     },
+                      //     icon: Image(
+                      //       image: AssetImage(sGoogleLogo),
+                      //       width: 20,
+                      //     ),
+                      //     label: Text(
+                      //       sLoginWithGoogle,
+                      //     )),
                     ),
                     TextButton(
                         onPressed: () => Get.to(() => LoginScreen()),
-                        child: Text.rich(
-                          TextSpan(
-                            children: [
-                              TextSpan(
+                        child: Text.rich(TextSpan(
+                          children: [
+                            TextSpan(
                                 text: sAlreadyHaveAnAccount,
-                                style: Theme.of(context).textTheme.bodyMedium
-                              ),
-                              TextSpan(
+                                style: Theme.of(context).textTheme.bodyMedium),
+                            TextSpan(
                                 text: sLoginText.toUpperCase(),
-                                style: Theme.of(context).textTheme.bodySmall
-                              )
-                            ],
-
-                          )
-                        ))
+                                style: Theme.of(context).textTheme.bodySmall)
+                          ],
+                        )))
                   ],
                 )
               ],
@@ -91,5 +80,3 @@ class SignUpScreen extends StatelessWidget {
     );
   }
 }
-
-
--- studybuddy/lib/src/repository/authentication_repository/authentication_repository.dart
+++ studybuddy/lib/src/repository/authentication_repository/authentication_repository.dart
@@ -1,15 +1,14 @@
-import 'package:flutter/material.dart';
-import  'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
+import 'package:get/get.dart';
+// import 'package:google_sign_in/google_sign_in.dart';
 import 'package:studybuddy/src/features/authentication/screens/onboarding_screens/onboarding_screen.dart';
 import 'package:studybuddy/src/repository/authentication_repository/exceptions/login_email_and_password_failure.dart';
 import 'package:studybuddy/src/repository/authentication_repository/exceptions/sign_up_email_and_password_failure.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../../features/authentication/screens/main_screens/main_screen.dart';
-import '../../features/authentication/screens/welcome_screens/landing_page.dart';
 import '../../features/authentication/screens/welcome_screens/welcome_screen.dart';
+
 class AuthenticationRepository extends GetxController {
   static AuthenticationRepository get instance => Get.find();
 
@@ -26,6 +25,7 @@ class AuthenticationRepository extends GetxController {
     ever(firebaseUser, _setInitialScreen);
     firebaseUser.bindStream(_auth.userChanges());
   } //The Rx puts the user in a stream and this user is a non-private variable
+
   //so we can reference it anywhere in the app
   late final Rx<User?> firebaseUser;
   //Defining methods to be used in the controllers here
@@ -42,11 +42,16 @@ class AuthenticationRepository extends GetxController {
   /**
    * Method to register a new user by creating a new user with email and password
    */
-  void registerUser(String email, String schoolName, String fullName,
-                    String password, String confirmPassword, ) async {
+  void registerUser(
+    String email,
+    String schoolName,
+    String fullName,
+    String password,
+    String confirmPassword,
+  ) async {
     try {
-      UserCredential userCredential = await _auth.createUserWithEmailAndPassword(
-          email: email, password: password);
+      UserCredential userCredential = await _auth
+          .createUserWithEmailAndPassword(email: email, password: password);
 
       // After successful registration, update the user's profile with their full name.
       await userCredential.user!.updateDisplayName(fullName);
@@ -59,14 +64,15 @@ class AuthenticationRepository extends GetxController {
         // Add other user data as needed.
       });
       //This checks if the user is null, if not, go to the landing page
-      firebaseUser.value != null ? Get.offAll(() => const MainScreen())
+      firebaseUser.value != null
+          ? Get.offAll(() => const MainScreen())
           : Get.offAll(() => OnBoardingScreen());
       print("attempting to register user");
-    }  on FirebaseAuthException catch (e) {
+    } on FirebaseAuthException catch (e) {
       final ex = SignUpEmailAndPasswordFailure.code(e.code);
       print('FIREBASE AUTH EXCEPTION: ${ex.message}');
       throw ex;
-    } catch(_) {
+    } catch (_) {
       const ex = SignUpEmailAndPasswordFailure();
       print('FIREBASE AUTH EXCEPTION: ${ex.message}');
       throw ex; //throwing the exception to the controller
@@ -88,8 +94,7 @@ class AuthenticationRepository extends GetxController {
       final ex = LoginEmailAndPasswordFailure.code(e.code);
       print('FIREBASE AUTH EXCEPTION: ${ex.message}');
       throw ex;
-    }
-    catch (_) {
+    } catch (_) {
       final ex = LoginEmailAndPasswordFailure();
       print('FIREBASE AUTH EXCEPTION: ${ex.message}');
       throw ex;
@@ -98,29 +103,27 @@ class AuthenticationRepository extends GetxController {
 
   Future<void> logout() async => await _auth.signOut();
 
-  Future<User?> signInWithGoogle() async {
-    final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-    if (googleUser == null) {
-      // The user canceled the sign-in process
-      return null;
-    }
-
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    final AuthCredential credential = GoogleAuthProvider.credential(
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-
-    try {
-      UserCredential authResult = await FirebaseAuth.instance.signInWithCredential(credential);
-      return authResult.user;
-    } catch (e) {
-      // Handle sign-in error
-      print("Error signing in with Google: $e");
-      return null;
-    }
-  }
-
-
+  // Future<User?> signInWithGoogle() async {
+  //   // final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  //
+  //   if (googleUser == null) {
+  //     // The user canceled the sign-in process
+  //     return null;
+  //   }
+  //
+  //   // final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+  //   // final AuthCredential credential = GoogleAuthProvider.credential(
+  //   //   accessToken: googleAuth.accessToken,
+  //   //   idToken: googleAuth.idToken,
+  //   // );
+  //
+  //   try {
+  //     UserCredential authResult = await FirebaseAuth.instance.signInWithCredential(credential);
+  //     return authResult.user;
+  //   } catch (e) {
+  //     // Handle sign-in error
+  //     print("Error signing in with Google: $e");
+  //     return null;
+  //   }
+  // }
 }
