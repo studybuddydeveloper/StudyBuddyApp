729156ece4f70ac77cad1851d22a2342661ba0d1
EUmoye <umoye22e@mtholyoke.edu>
2024-03-18 19:58:15 -0400

    setting up the UI for the time selection piece

--- studybuddy/lib/src/reusable_widgets/AvailabilityTimeWidget.dart
+++ studybuddy/lib/src/reusable_widgets/AvailabilityTimeWidget.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import '../features/authentication/screens/widgets/appbar-widget.dart';
 
 class ScheduleGridWidget extends StatefulWidget {
-  // print("hello world");
   @override
   _ScheduleGridWidgetState createState() => _ScheduleGridWidgetState();
 }
@@ -20,32 +19,286 @@ class _ScheduleGridWidgetState extends State<ScheduleGridWidget> {
   Widget build(BuildContext context) {
     items = createListOfTimes(selectedStartTime, selectedEndTime);
     return Builder(
-        builder: (context) =>
-            Scaffold(
+        builder: (context) => Scaffold(
               appBar: buildAppBar(context),
-              body: Row(
-                children: [
-                  // Day of the week (e.g., Monday)
-                  Text('Monday'),
-                  SizedBox(width: 10), // Spacer between text and dropdowns
-                  // Start time dropdown button
-                  buildDropdownButton(selectedStartTime, (newValue) {
-                    setState(() {
-                      selectedStartTime = newValue;
-                    });
-                  }),
-                  SizedBox(width: 10), // Spacer between dropdowns
-                  // Text 'till'
-                  Text('till'),
-                  SizedBox(width: 10), // Spacer between text and dropdowns
-                  // End time dropdown button
-                  buildDropdownButton(selectedEndTime, (newValue) {
-                    setState(() {
-                      selectedEndTime = newValue;
-                    });
-                    // TODO: Check if the value for the start and end times selected is the same
-                  }),
-                ],
+              body: Scrollbar(
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                  alignment: Alignment.center,
+                  child: Column(
+                    // crossAxisAlignment: CrossAxisAlignment.stretch,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          // crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                                child: Text("Monday"),
+                                style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.white,
+                                    backgroundColor: Colors.black,
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 40, vertical: 30),
+                                    shape: BeveledRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                                // key: "Sunday",
+                                onHover: (True) => {},
+                                onPressed: () => {}),
+                            SizedBox(width: 50),
+                            // Spacer between text and dropdowns
+                            // Start time dropdown button
+                            buildDropdownButton(selectedStartTime, (newValue) {
+                              setState(() {
+                                selectedStartTime = newValue;
+                              });
+                            }),
+                            SizedBox(width: 10),
+                            // Spacer between dropdowns
+                            // Text 'till'
+                            Text('till'),
+                            SizedBox(width: 10),
+                            buildDropdownButton(selectedEndTime, (newValue) {
+                              setState(() {
+                                selectedEndTime = newValue;
+                              });
+                              // Spacer between text and dropdowns
+                              // End time dropdown button
+                            })
+                            // End time dropdown button
+                          ]),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(
+                              child: Text("Tuesday"),
+                              style: ElevatedButton.styleFrom(
+                                  foregroundColor: Colors.white,
+                                  backgroundColor: Colors.black,
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 40, vertical: 30),
+                                  shape: BeveledRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                              // key: "Sunday",
+                              onHover: (True) => {},
+                              onPressed: () => {}),
+                          SizedBox(width: 10),
+                          // Start time dropdown button
+                          buildDropdownButton(selectedStartTime, (newValue) {
+                            setState(() {
+                              selectedStartTime = newValue;
+                            });
+                          }),
+                          SizedBox(width: 10),
+                          // Spacer between dropdowns
+                          // Text 'till'
+                          Text('till'),
+                          SizedBox(width: 10),
+                          // Spacer between text and dropdowns
+                          // End time dropdown button
+                          buildDropdownButton(selectedEndTime, (newValue) {
+                            setState(() {
+                              selectedEndTime = newValue;
+                            });
+                          })
+                        ],
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          ElevatedButton(
+                              child: Text("Wednesday"),
+                              style: ElevatedButton.styleFrom(
+                                  foregroundColor: Colors.white,
+                                  backgroundColor: Colors.black,
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 40, vertical: 30),
+                                  shape: BeveledRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                              // key: "Sunday",
+                              onHover: (True) => {},
+                              onPressed: () => {}),
+                          SizedBox(width: 10),
+                          // Spacer between text and dropdowns
+                          // Start time dropdown button
+                          buildDropdownButton(selectedStartTime, (newValue) {
+                            setState(() {
+                              selectedStartTime = newValue;
+                            });
+                          }),
+                          SizedBox(width: 10),
+                          // Spacer between dropdowns
+                          // Text 'till'
+                          Text('till'),
+                          SizedBox(width: 10),
+                          // Spacer between text and dropdowns
+                          // End time dropdown button
+                          buildDropdownButton(selectedEndTime, (newValue) {
+                            setState(() {
+                              selectedEndTime = newValue;
+                            });
+                          })
+                        ],
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            ElevatedButton(
+                                child: Text("Thursday"),
+                                style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.white,
+                                    backgroundColor: Colors.black,
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 40, vertical: 30),
+                                    shape: BeveledRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                                // key: "Sunday",
+                                onHover: (True) => {},
+                                onPressed: () => {}),
+                            SizedBox(width: 10),
+
+                            buildDropdownButton(selectedStartTime, (newValue) {
+                              setState(() {
+                                selectedStartTime = newValue;
+                              });
+                            }),
+                            SizedBox(width: 10),
+                            // Spacer between dropdowns
+                            // Text 'till'
+                            Text('till'),
+                            SizedBox(width: 10),
+                            // Spacer between text and dropdowns
+                            // End time dropdown button
+                            buildDropdownButton(selectedEndTime, (newValue) {
+                              setState(() {
+                                selectedEndTime = newValue;
+                              });
+                            })
+                          ]),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          // Day of the week (e.g., Monday)
+                          ElevatedButton(
+                              child: Text("Friday"),
+                              style: ElevatedButton.styleFrom(
+                                  foregroundColor: Colors.white,
+                                  backgroundColor: Colors.black,
+                                  padding: EdgeInsets.symmetric(
+                                      horizontal: 40, vertical: 30),
+                                  shape: BeveledRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10))),
+                              // key: "Sunday",
+                              onHover: (True) => {},
+                              onPressed: () => {}),
+                          SizedBox(width: 10),
+                          // Spacer between text and dropdowns
+                          // Start time dropdown button
+                          buildDropdownButton(selectedStartTime, (newValue) {
+                            setState(() {
+                              selectedStartTime = newValue;
+                            });
+                          }),
+                          SizedBox(width: 10),
+                          // Spacer between dropdowns
+                          // Text 'till'
+                          Text('till'),
+                          SizedBox(width: 10),
+                          // Spacer between text and dropdowns
+
+                          buildDropdownButton(selectedEndTime, (newValue) {
+                            setState(() {
+                              selectedEndTime = newValue;
+                            });
+                            // TODO: Check if the value for the start and end times selected is the same
+                            // End time dropdown button
+                          })
+                        ],
+                      ),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          // crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                                child: Text("Saturday"),
+                                style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.white,
+                                    backgroundColor: Colors.black,
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 40, vertical: 30),
+                                    shape: BeveledRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                                // key: "Sunday",
+                                onHover: (True) => {},
+                                onPressed: () => {}),
+                            SizedBox(width: 50),
+                            // Spacer between text and dropdowns
+                            // Start time dropdown button
+                            buildDropdownButton(selectedStartTime, (newValue) {
+                              setState(() {
+                                selectedStartTime = newValue;
+                              });
+                            }),
+                            SizedBox(width: 10),
+                            // Spacer between dropdowns
+                            // Text 'till'
+                            Text('till'),
+                            SizedBox(width: 10),
+                            buildDropdownButton(selectedEndTime, (newValue) {
+                              setState(() {
+                                selectedEndTime = newValue;
+                              });
+                              // Spacer between text and dropdowns
+                              // End time dropdown button
+                            })
+                            // End time dropdown button
+                          ]),
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          // crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            ElevatedButton(
+                                child: Text("Sunday"),
+                                style: ElevatedButton.styleFrom(
+                                    foregroundColor: Colors.white,
+                                    backgroundColor: Colors.black,
+                                    padding: EdgeInsets.symmetric(
+                                        horizontal: 40, vertical: 30),
+                                    shape: BeveledRectangleBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(10))),
+                                // key: "Sunday",
+                                onHover: (True) => {},
+                                onPressed: () => {}),
+                            // Text("Sunday"),
+                            SizedBox(width: 50),
+                            // Spacer between text and dropdowns
+                            // Start time dropdown button
+                            buildDropdownButton(selectedStartTime, (newValue) {
+                              setState(() {
+                                selectedStartTime = newValue;
+                              });
+                            }),
+                            SizedBox(width: 10),
+                            // Spacer between dropdowns
+                            // Text 'till'
+                            Text('till'),
+                            SizedBox(width: 10),
+                            buildDropdownButton(selectedEndTime, (newValue) {
+                              setState(() {
+                                selectedEndTime = newValue;
+                              });
+                              // Spacer between text and dropdowns
+                              // End time dropdown button
+                            })
+                            // End time dropdown button
+                          ]),
+                    ],
+                  ),
+                ),
               ),
             ));
   }
@@ -53,12 +306,11 @@ class _ScheduleGridWidgetState extends State<ScheduleGridWidget> {
   /**
    * It takes in a selected value, and creates a drop down of times from that value.This is in increments of 30 minutes!
    */
-  Widget buildDropdownButton(String selectedValue,
-      ValueChanged<String> onChanged) {
+  Widget buildDropdownButton(
+      String selectedValue, ValueChanged<String> onChanged) {
     return DropdownButton<String>(
       value: selectedValue,
-      onChanged: (value) =>
-      {
+      onChanged: (value) => {
         // TODO: CHange this value to the updated value when it's changed on the screen
       },
       items: items.map((value) {
@@ -71,6 +323,9 @@ class _ScheduleGridWidgetState extends State<ScheduleGridWidget> {
   }
 }
 
+/**
+ * This function creates a list of times from the start time to the end time
+ */
 List<String> createListOfTimes(String startTime, String endTime) {
   List<String> times = [];
 
